1000b8985d8791fd8f9a8654d66d7fba9aee1ce6
Bruno <brunofvalli@gmail.com>
2024-03-14 11:06:46 -0500

    +Changes added

---
+++ EnergySmartBridge/Modules/ConnectingFailedHandlerCustom.cs
@@ -0,0 +1,22 @@
+﻿using MQTTnet.Extensions.ManagedClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergySmartBridge.Modules
+{
+    public class ConnectingFailedHandlerCustom : IConnectingFailedHandler
+    {
+        public Task HandleConnectingFailedAsync(ManagedProcessFailedEventArgs handler)
+        {
+            var handlerJson = Newtonsoft.Json.JsonConvert.SerializeObject(handler);
+
+            Console.WriteLine($"ConnectingFailedHandlerCustom.HandleConnectingFailedAsync: {handlerJson}");
+
+            return null;   
+        }
+    }
+}
---
+++ EnergySmartBridge/Modules/MqttClientDisconnectedHandlerCustom.cs
@@ -0,0 +1,22 @@
+﻿using MQTTnet.Client.Disconnecting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergySmartBridge.Modules
+{
+    internal class MqttClientDisconnectedHandlerCustom : IMqttClientDisconnectedHandler
+    {
+        public Task HandleDisconnectedAsync(MqttClientDisconnectedEventArgs handler)
+        {
+            var handlerJson = Newtonsoft.Json.JsonConvert.SerializeObject(handler);
+
+            Console.WriteLine($"ConnectingFailedHandlerCustom.HandleConnectingFailedAsync: {handlerJson}");
+
+            return null;
+        }
+    }
+}
